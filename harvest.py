2eaf3e4b49ae1f8901f23abf667f250485743e0c
Federico <f.r.rubini@uu.nl>
2022-09-27 16:31:36 +0200

    added the library as submodule

--- harvest.py
+++ harvest.py
@@ -2,7 +2,7 @@ import datetime
 import csv
 
 from oaipmh.client import Client
-from oaipmh.metadata import MetadataRegistry, oai_dc_reader
+from oaipmh.metadata import MetadataRegistry, MetadataReader
 
 
 def main():
@@ -13,6 +13,34 @@ def main():
     until_date = datetime.datetime.now()
     from_date = until_date - datetime.timedelta(hours=16)
     registry = MetadataRegistry()
+
+    # instantiate the oai_Reader with the right metadata
+    oai_dc_reader = MetadataReader(
+        fields={
+            'title': ('textList', 'oai_dc:dc/dc:title/text()'),
+            'creator': ('textList', 'oai_dc:dc/dc:creator/text()'),
+            'subject': ('textList', 'oai_dc:dc/dc:subject/text()'),
+            'description': ('textList', 'oai_dc:dc/dc:description/text()'),
+            'publisher': ('textList', 'oai_dc:dc/dc:publisher/text()'),
+            'contributor': ('textList', 'oai_dc:dc/dc:contributor/text()'),
+            'date': ('textList', 'oai_dc:dc/dc:date/text()'),
+            'type': ('textList', 'oai_dc:dc/dc:type/text()'),
+            'format': ('textList', 'oai_dc:dc/dc:format/text()'),
+            'identifier': ('textList', 'oai_dc:dc/dc:identifier/text()'),
+            'source': ('textList', 'oai_dc:dc/dc:source/text()'),
+            'language': ('textList', 'oai_dc:dc/dc:language/text()'),
+            'relation': ('textList', 'oai_dc:dc/dc:relation/text()'),
+            'coverage': ('textList', 'oai_dc:dc/dc:coverage/text()'),
+            'rights': ('textList', 'oai_dc:dc/dc:rights/text()'),
+            'ispartof': ('textList', 'oai_dc:dc/dc:isPartOf/text()')
+        },
+        namespaces={
+            'oai_dc': 'http://www.openarchives.org/OAI/2.0/oai_dc/',
+            'dc': 'http://purl.org/dc/elements/1.1/'}
+    )
+
+
+
     registry.registerReader('oai_dc', oai_dc_reader)
     client = Client(URL, registry)
 
