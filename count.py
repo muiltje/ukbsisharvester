4ec83ef83b2175027556d53aa88a90cf66b275d0
Federico <f.r.rubini@uu.nl>
2022-10-13 11:37:28 +0200

    Added count of available records to harvest

---
+++ count.py
@@ -0,0 +1,58 @@
+import csv
+
+from dateutil.rrule import rrule, MONTHLY
+from datetime import datetime
+
+from oaipmh.client import Client
+from oaipmh.metadata import MetadataRegistry, oai_dc_reader
+
+
+def count_articles():
+    URL = 'http://oai.narcis.nl/oai'
+    registry = MetadataRegistry()
+
+    start = datetime.fromisoformat('2022-01-01')
+    end = datetime.fromisoformat('2022-10-01')
+
+    print(">>>> START >>>> Counting records ", start.strftime('%d-%m-%Y'), end.strftime('%d-%m-%Y'))
+
+    registry.registerReader('oai_dc', oai_dc_reader)
+    client = Client(URL, registry)
+
+    with open('_count.csv', 'w', newline='') as f:
+        fieldnames = ['from', 'to', 'deleted', 'count']
+        writer = csv.DictWriter(f, fieldnames=fieldnames, delimiter=';', quotechar='"')
+        # writer.writeheader()
+
+        prev = start
+        for dt in rrule(freq=MONTHLY, dtstart=start, until=end)[1:]:
+            deleted = 0
+            count = 0
+            print("-> Getting records:", prev.strftime('%d-%m-%Y'), dt.strftime('%d-%m-%Y'), deleted, count)
+
+            try:
+                records = client.listRecords(metadataPrefix='oai_dc', from_=prev, until=dt, set='publication')
+
+                # total = sum(1 for _ in records)
+                # return (total)
+
+                for num, record in enumerate(records):
+                    if record[0].isDeleted():
+                        deleted = deleted + 1
+                    elif not record[1] is None:
+                        count = count + 1
+                    else:
+                        print("something strange")
+            except:
+                print(">> no records found")
+
+            print("    - Found records", deleted, count)
+
+            writer.writerow(
+                {'from': prev.strftime('%d-%m-%Y'), 'to': dt.strftime('%d-%m-%Y'), 'deleted': deleted, 'count': count})
+
+            prev = dt
+
+
+if __name__ == '__main__':
+    count_articles()
