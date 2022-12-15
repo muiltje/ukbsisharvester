d50d4bee9dbc0800152c628db00e259a5f9aa1dd
Federico <f.r.rubini@uu.nl>
2022-12-15 09:40:11 +0100

    updated comments

--- harvest_initial.py
+++ harvest_initial.py
@@ -1,8 +1,12 @@
 from harvest import initial_harvest
 
 """
-This makes initial harvest until today
-"""
+Harvest of all historical data until today
 
+consists in a monthly harvest until current month and then a normal harvest until yesterday
+
+date_from : should be in ISO format: YYYY-MM-DD, any date can be used
+
+"""
 if __name__ == '__main__':
-    initial_harvest('2020-05-01')
+    initial_harvest(date_from='2020-05-01')
